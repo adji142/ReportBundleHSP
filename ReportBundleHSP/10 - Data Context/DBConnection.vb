69ede3d87d42960307fc14a584efdd62a165babc
adji142 <prasetyoajiw@gmail.com>
2019-03-05 10:42:55 +0700

    initial commit

---
+++ ReportBundleHSP/10 - Data Context/DBConnection.vb
@@ -0,0 +1,55 @@
+﻿Imports MySql.Data.MySqlClient
+Imports System.ComponentModel
+
+Namespace HSP.Data
+    Public Class DBConnection
+        Private _ConnectionString As String = ""
+
+        Sub New()
+            Dim Temp As New MySqlConnectionStringBuilder
+
+            Temp.Server = My.Settings.Server
+            Temp.UserID = My.Settings.UserID
+            Temp.Password = My.Settings.Password
+            Temp.Database = My.Settings.DatabaseName
+            Temp.Port = My.Settings.Port
+
+            _ConnectionString = Temp.ConnectionString
+        End Sub
+
+        'Overload Konstruktor
+        Sub New(ByVal Session As Object)
+            If Session.DBConnection.Trim() <> "" Then
+                _ConnectionString = Session.DBConnection
+            Else
+                Dim Temp As New MySqlConnectionStringBuilder
+
+                Temp.Server = My.Settings.Server
+                Temp.UserID = My.Settings.UserID
+                Temp.Password = My.Settings.Password
+                Temp.Database = My.Settings.DatabaseName
+                Temp.Port = My.Settings.Port
+
+                _ConnectionString = Temp.ConnectionString
+            End If
+        End Sub
+
+        'Koneksi Dengan Database Server
+        Public Function Connection() As MySqlConnection
+            Dim DBX As New MySqlConnection
+
+            DBX.ConnectionString = _ConnectionString
+            DBX.Open()
+            Connection = DBX
+
+        End Function
+
+        Public Function ConnectionSetting() As Object
+            Dim Temp As New MySqlConnectionStringBuilder
+            Temp.ConnectionString = _ConnectionString
+            ConnectionSetting = Temp
+        End Function
+
+    End Class
+
+End Namespace
---
+++ ReportBundleHSP/Report/FrmPengeluaranBarang.vb
@@ -0,0 +1,3 @@
+﻿Public Class FrmPengeluaranBarang
+
+End Class
