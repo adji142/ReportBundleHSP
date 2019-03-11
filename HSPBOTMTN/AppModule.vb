76bd06652628d96de36ef6fd03eea8a8ee3b72fc
adji142 <prasetyoajiw@gmail.com>
2019-03-11 14:50:19 +0700

    add project HSPBOTMTN

---
+++ HSPBOTMTN/AppModule.vb
@@ -0,0 +1,47 @@
+﻿Public Class AppModule
+
+    'Get Plugin Modul List
+    Public Function GetAppModule() As DataTable
+        Dim oDS As New DataSet
+        Dim oDT As DataTable
+        Dim oApp As DataRow
+
+        oDT = oDS.Tables.Add("AppModule")
+        oDT.Columns.Add("PluginKeywordID")
+        oDT.Columns.Add("AppDescription")
+
+        '-----------------------------------------------------------------------------------
+        oApp = oDT.Rows.Add()
+        oApp(0) = "MTN01"
+        oApp(1) = "MTN01 - Pemakaian Obat"
+        '-----------------------------------------------------------------------------------
+
+        '-----------------------------------------------------------------------------------
+        oApp = oDT.Rows.Add()
+        oApp(0) = "MTN02"
+        oApp(1) = "MTN02 - Pemakaian ATK"
+        '-----------------------------------------------------------------------------------
+
+        oDS.AcceptChanges()
+        GetAppModule = oDT
+
+    End Function
+
+    'Execute Plugin
+    Public Sub Execute(ByVal ID As String, Server As Object)
+        Dim Modul As Object = Nothing
+
+        '-------------------------------------------------------------------------
+        Select Case ID
+            Case "MTN01"
+                Modul = New Plugin_PemakaianObat(Server)
+                Modul.Execute()
+            Case "MTN02"
+                Modul = New Plugin_PemakaianATK(Server)
+                Modul.Execute()
+        End Select
+        '-------------------------------------------------------------------------
+
+    End Sub
+
+End Class
---
+++ HSPBOTMTN/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim X As New Plugin_PemakaianObat(Nothing)
+        X._Create()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim X As New Plugin_PemakaianATK(Nothing)
+        X._Create()
+    End Sub
+End Class
